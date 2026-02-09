b7e1d73d4634f9642566f03d3c5dcba663d6521b
urfan <urfan.laode@gmail.com>
2026-02-09 08:53:56 +0800

    fix(stories): Unable modify label in select-dropdown

--- src/components/ui/select-dropdown/select-dropdown.tsx
+++ src/components/ui/select-dropdown/select-dropdown.tsx
@@ -65,7 +65,7 @@ const SelectDropdown = forwardRef<HTMLButtonElement, SelectDropdownProps>(
       withPortal = true,
       onOpen,
       onClose,
-      label,
+      label = 'Label',
     },
     ref
   ) => {
